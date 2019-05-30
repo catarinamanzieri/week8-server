ef37d080e7db00d981dbd923a0921db31a618ac7
CataMZ <catarina.manzieri@gmail.com>
2019-05-29 22:02:49 -0300

    Fazer estrutura da requisição GET

--- server.js
+++ server.js
@@ -1,15 +1,72 @@
 const http = require('http') 
 
+const comidas = {
+    pratosFavoritos: [
+    {
+        nome: "Batata frita",
+        descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+        imagem: "img/Batata-frita.jpg"
+    },
+    {
+        nome: "Macarronada",
+        descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+        imagem: "img/macarronada.jpg"
+    },
+    {
+        nome: "Paçoquita",
+        descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+        imagem: "https://http2.mlstatic.com/pacoquita-de-amendoim-caixa-com-20g-cada-pacoca-premium-D_NQ_NP_917716-MLB27126685393_042018-F.jpg"
+    },
+    {
+        nome: "Batata frita",
+        descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+        imagem: "img/Batata-frita.jpg"
+    },
+    {
+        nome: "Macarronada",
+        descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+        imagem: "img/macarronada.jpg"
+    },
+    {
+        nome: "Paçoquita",
+        descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+        imagem: "https://http2.mlstatic.com/pacoquita-de-amendoim-caixa-com-20g-cada-pacoca-premium-D_NQ_NP_917716-MLB27126685393_042018-F.jpg"
+    },
+    {
+        nome: "Batata frita",
+        descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+        imagem: "img/Batata-frita.jpg"
+    },
+    {
+        nome: "Macarronada",
+        descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+        imagem: "img/macarronada.jpg"
+    },
+    {
+        nome: "Paçoquita",
+        descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+        imagem: "https://http2.mlstatic.com/pacoquita-de-amendoim-caixa-com-20g-cada-pacoca-premium-D_NQ_NP_917716-MLB27126685393_042018-F.jpg"
+    },
+    {
+        nome: "Batata frita",
+        descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+        imagem: "img/Batata-frita.jpg"
+    }]
+}
+
 const servidor = http.createServer(function (request, response){
-	if (request.url === '/'){
+    if (request.url === '/'){
         response.end("Inhaii mundo lindjo")
     } else if (request.url === '/comidas'){
         // response.end("Resposta diferente")
         if (request.method === 'GET'){
             response.writeHead(200, {
-                "Content-Type": "text/html;charset=utf-8"
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
             })
-            response.end("<h1>O que tocar no coração do GET</h1>")
+            response.write(JSON.stringify(comidas))
+            // response.end("<h1>O que tocar no coração do GET</h1>")
+            response.end()
         } else if(request.method === 'POST'){
             response.writeHead(201, {
                 "Content-Type": "text/html;charset=utf-8"
