ad9997b991b68469160479d37972162a0dd540dc
CataMZ <catarina.manzieri@gmail.com>
2019-05-29 22:31:45 -0300

    Instalar Express e fazer requisição GET

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require('http') 
+const express = require('express') 
 
 const comidas = {
     pratosFavoritos: [
@@ -54,27 +54,33 @@ const comidas = {
     }]
 }
 
-const servidor = http.createServer(function (request, response){
-    if (request.url === '/'){
-        response.end("Inhaii mundo lindjo")
-    } else if (request.url === '/comidas'){
-        // response.end("Resposta diferente")
-        if (request.method === 'GET'){
-            response.writeHead(200, {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*"
-            })
-            response.write(JSON.stringify(comidas))
-            // response.end("<h1>O que tocar no coração do GET</h1>")
-            response.end()
-        } else if(request.method === 'POST'){
-            response.writeHead(201, {
-                "Content-Type": "text/html;charset=utf-8"
-            })
-            response.end("<h1>O que tocar no coração do POST</h1>")
-        }
-    }
+const servidor = express()
+
+servidor.get("/comidas", (request, response) => {
+    response.send("Inhaí")
 })
+
+// const servidor = http.createServer(function (request, response){
+//     if (request.url === '/'){
+//         response.end("Inhaii mundo lindjo")
+//     } else if (request.url === '/comidas'){
+//         // response.end("Resposta diferente")
+//         if (request.method === 'GET'){
+//             response.writeHead(200, {
+//                 "Content-Type": "application/json",
+//                 "Access-Control-Allow-Origin": "*"
+//             })
+//             response.write(JSON.stringify(comidas))
+//             // response.end("<h1>O que tocar no coração do GET</h1>")
+//             response.end()
+//         } else if(request.method === 'POST'){
+//             response.writeHead(201, {
+//                 "Content-Type": "text/html;charset=utf-8"
+//             })
+//             response.end("<h1>O que tocar no coração do POST</h1>")
+//         }
+//     }
+// })
 
 servidor.listen(3000)
 console.log("Servidor rodando na porta 3000")
